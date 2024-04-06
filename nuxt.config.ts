703616644ae93d821bb3dbbfdd2c7b6ea5c905ea
zmzlois <lois.z@comcord.vision>
2024-04-06 18:57:50 +0100

    add transition css at global config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  app: {
+    pageTransition: {
+      name: "v",
+      mode: "out-in",
+    },
+    head: {
+      bodyAttrs: {
+        class: "bg-background",
+      },
+    },
+  },
   tailwindcss: {
     cssPath: "./assets/css/tailwind.css",
     configPath: "tailwind.config.ts",
