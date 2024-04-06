55dee1e6daa0f7c83b6cff342cc5809cc985fd79
zmzlois <lois.z@comcord.vision>
2024-04-06 18:57:29 +0100

    tailwind indicator

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
     <NuxtLayout />
+    <ClientOnly placeholder="Loading..." fallback-tag="span" v-show="isDev">
+      <TailwindIndicator />
+    </ClientOnly>
   </div>
 </template>
+<script>
+export default {
+  setup() {
+    let isDev = true;
+    const environment = process.env.NODE_ENV;
+    console.log("environment:", environment);
+
+    if (environment === "development") {
+      isDev = true;
+    } else {
+      isDev = false;
+    }
+    return { isDev };
+  },
+};
+</script>
